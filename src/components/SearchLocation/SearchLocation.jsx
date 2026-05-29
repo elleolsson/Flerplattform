3b84604796b922a76b0a20345ad2ec9f7a94146f
Woxell <andre@woxell.se>
2026-05-29 17:56:15 +0200

    Adjust some css for better fit in mobile view

--- src/components/SearchLocation/SearchLocation.jsx
+++ src/components/SearchLocation/SearchLocation.jsx
@@ -4,7 +4,7 @@ import './SearchLocation.css';
 export default function SearchLocation() {
     return (
         <div className="search-location">
-            <Button variant="info">Använd min plats</Button>
+            <Button variant="info" size="sm">Använd min plats</Button>
             {/*<label htmlFor="location-input">Adress:</label>*/}
             <input type="text" name="" id="location-input" placeholder="Ange din adress här" />
         </div>
