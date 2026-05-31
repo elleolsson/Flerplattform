9c9ba893261a11ce79969488f613a08b093986ec
emilaqvist <emilaqvist@gmail.com>
2026-05-31 15:44:38 +0200

    Spara knapp till adressök.
    La till en spara knapp till adressökningsfältet främst för användarvänlighet där användaren ska upplevas att deras svar "sparas".
    
    Sen också att den "avsparas" när man gör ändringar i fältet igen och lade ny funktion för det.

--- src/components/SearchLocation/SearchLocation.jsx
+++ src/components/SearchLocation/SearchLocation.jsx
@@ -1,12 +1,26 @@
 import { Button } from "react-bootstrap";
 import './SearchLocation.css';
+import { useState } from "react";
 
 export default function SearchLocation() {
+
+    //Funktion som händer varje gång en ändring hädner i adressens fält. (ska användas till mer grejer framöver).
+    const addrInputChange = ()=>{
+        setSaveButtonState(true);
+    }
+    /*Ny use state för en knapp som förbättrar användarupplevelsen genom att placeras vid 
+    adressfältet och ändrar färg/text vid klick. Ger upplevelsen att svaret sparas.*/
+    const [saveButtonState, setSaveButtonState] = useState(true); 
     return (
         <div className="search-location">
             <Button variant="info" size="sm">Använd min plats</Button>
             {/*<label htmlFor="location-input">Adress:</label>*/}
-            <input type="text" name="" id="location-input" placeholder="Ange din adress här" />
+            <input type="text" name="" id="location-input" placeholder="Ange din adress här" onChange={addrInputChange}/>
+            <Button
+                variant={saveButtonState ? "info" : "dark"}
+                onClick={()=>setSaveButtonState(false)}
+                size="sm"
+            >{saveButtonState ? "Spara" : "Sparat"}</Button>
         </div>
     )
 }
