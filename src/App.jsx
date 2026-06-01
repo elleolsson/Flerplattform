fa00b2fd02480c8ef80c2d4eff4c921aadcefa6e
Woxell <andre@woxell.se>
2026-06-01 16:15:23 +0200

    Remove unused components from App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
 import TabSearch from './components/TabSearch/TabSearch'
 import TabFavourites from './components/TabFavourites/TabFavourites'
-import SearchRadius from './components/SearchRadius/SearchRadius'
-import SearchLocation from './components/SearchLocation/SearchLocation'
 import { Button } from 'react-bootstrap'
 import './App.css'
 
